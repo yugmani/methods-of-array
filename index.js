7fff4146773abb8063dffa121081eaa2eb706ea3
yugmani <yugmani@yahoo.com>
2021-05-05 13:17:41 -0700

    Added more array mothods

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const items = [
 const displayItems = (arr, code, method) => {
   let html = `<h3><span class='method-name'>${method}</span> Method</h3>
   <h4>Code Sample:</h4>
-  <mark>${code}</mark>
+  <div class="mark">${code}</div>
   <ul>`;
 
   arr.map(item => {
@@ -55,8 +55,6 @@ document.getElementById("map").innerHTML = `${mapItems}`;
 //FIND METHOD
 const foundItems = items.find(item => item.name === "Mouse");
 
-console.log("find:", foundItems);
-
 const findCode = `items.find( item => item.name === "Mouse" );`;
 
 // const findItems = displayItems(foundItems, findCode, "find Method");
@@ -64,19 +62,19 @@ const findCode = `items.find( item => item.name === "Mouse" );`;
 document.getElementById("find").innerHTML = `
 <h3><span class='method-name'>find</span> Method</h3>
 <h4>Code Sample:</h4>
-<mark>${findCode}</mark>
+<div class="mark">${findCode}</div>
  <ul><li><span class="name">${foundItems.name}
 <span class="price">${foundItems.price}</span></li></ul>
 `;
 
 //FOR EACH METHOD
-let eachCode = `items.forEach(item=>console.log(item.name));`;
+let eachCode = `items.forEach(item=>console.log(item));`;
 
 const forE = arr => {
   let html = `
 <h3><span class='method-name'>forEach</span> Method</h3>
 <h4>Code Sample:</h4>
-<mark>${eachCode}</mark>
+<div class="mark">${eachCode}</div>
 <ul>`;
   arr.forEach(item => {
     html += `<li>
@@ -94,12 +92,12 @@ document.getElementById("each").innerHTML = forE(items);
 //SOME Method
 const hasCheapItems = items.some(item => item.price < 100);
 
-const someCode = `items.some( item => item.name < 100 );`;
+const someCode = `items.some( item => item.price < 100 );`;
 
 document.getElementById("some").innerHTML = `
 <h3><span class='method-name'>some</span> Method</h3>
 <h4>Code Sample:</h4>
-<mark>${someCode}</mark>
+<div class="mark">${someCode}</div>
  <ul><li><span class="name">${hasCheapItems}
 </li></ul>
 `;
@@ -112,7 +110,7 @@ const everyCode = `items.every( item => item.price < 100 );`;
 document.getElementById("every").innerHTML = `
 <h3><span class='method-name'>every</span> Method</h3>
 <h4>Code Sample:</h4>
-<mark>${everyCode}</mark>
+<div class="mark">${everyCode}</div>
  <ul><li><span class="name">${everyItems}
 </li></ul>
 `;
@@ -129,7 +127,55 @@ const reduceCode = `items.reduce((total, item) => {
 document.getElementById("reduce").innerHTML = `
 <h3><span class='method-name'>reduce</span> Method</h3>
 <h4>Code Sample:</h4>
-<mark>${reduceCode}</mark>
+<div class="mark">${reduceCode}</div>
  <ul><li>Total Price:<span class="price">${totalPrice}
 </li></ul>
+`;
+
+//INCLUDES method
+const array = [1, 2, 3, 4, 5];
+const includesTwo = array.includes(2);
+const includesSeven = array.includes(7);
+
+const includesCode = `
+const array = [1, 2, 3, 4, 5];<br>
+const includesTwo = array.includes(2);<br>
+const includesSeven = array.includes(7);<br>
+console.log("array includes 2: ", includesTwo);<br>
+console.log("array includes 7: ", includesSeven);
+`;
+
+document.getElementById("include").innerHTML = `
+<h3><span class='method-name'>includes</span> Method</h3>
+<h4>Code Sample:</h4>
+<div class="mark">${includesCode}</div>
+ <ul>
+ <li>array includes 2: <span class="name">${includesTwo}</li>
+ <li>array includes 7: <span class="name">${includesSeven}</li>
+</ul>
+`;
+
+//INCLUDES method
+const fruits = ["mango", "apple", "orange", "banana", "Avocado"];
+const includesApple = fruits.includes("apple");
+const includesBerry = fruits.includes("berry");
+console.log(includesApple);
+console.log(includesBerry);
+
+const includesCode2 = `
+const fruits = ["mango", "apple", "orange", "banana", "Avocado"];<br>
+const includesApple = fruits.includes("apple");<br>
+const includesBerry = fruits.includes("berry");<br>
+console.log("fruits includes Apple: ", includesApple);<br>
+console.log("fruits includes Berry: ", includesBerry);
+`;
+
+document.getElementById("include2").innerHTML = `
+<h3><span class='method-name'>includes</span> Method</h3>
+<h4>Code Sample:</h4>
+<div class="mark">${includesCode2}</div>
+ <ul>
+ <li>fruits includes 2: <span class="name">${includesApple}</li>
+ <li>fruits includes 7: <span class="name">${includesBerry}</li>
+</ul>
 `;
